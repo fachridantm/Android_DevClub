a36db891014c12296a21cb52a10d37d3b09ebe00
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2021-05-19 19:40:48 +0700

    Update adapter, retrofit

--- Week4/app/src/main/java/com/adc/week4/adapter/ListNewsAdapter.kt
+++ Week4/app/src/main/java/com/adc/week4/adapter/ListNewsAdapter.kt
@@ -1,4 +1,51 @@
 package com.adc.week4.adapter
 
-class ListNewsAdapter {
-}
+import com.adc.week4.data.api.response.ArticlesItem
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.adc.week4.R
+import com.adc.week4.utils.setImageUrl
+
+class ListNewsAdapter (
+    val context: Context,
+    val data: MutableList<ArticlesItem> = mutableListOf(),
+    val listener: OnUserItemListener? = null
+    ) : RecyclerView.Adapter<ListNewsAdapter.ListNewsViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListNewsViewHolder {
+            return ListNewsViewHolder(LayoutInflater.from(context).inflate(R.layout.item_row_news, parent, false))
+        }
+
+        override fun getItemCount(): Int = data.size
+
+        override fun onBindViewHolder(holder: ListNewsViewHolder, position: Int) {
+            holder.bind(data[position])
+        }
+
+        inner class ListNewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var tvTitle: TextView = itemView.findViewById(R.id.tv_title)
+            var tvNews: TextView = itemView.findViewById(R.id.tv_news)
+            var ivNews: ImageView = itemView.findViewById(R.id.iv_news)
+            var pbNews: ProgressBar = itemView.findViewById(R.id.pb_news)
+            fun bind(data: ArticlesItem) {
+                with(itemView) {
+                    ivNews.setImageUrl(context, data.urlToImage, pbNews)
+                    tvTitle.text = data.title
+                    tvNews.text = data.description
+                    setOnClickListener {
+                        listener?.onUserItemClicked(data)
+                    }
+                }
+            }
+        }
+
+        interface OnUserItemListener {
+            fun onUserItemClicked(data: ArticlesItem)
+        }
+    }
--- Week4/app/src/main/java/com/adc/week4/data/api/NewsApi.kt
+++ Week4/app/src/main/java/com/adc/week4/data/api/NewsApi.kt
@@ -1,12 +1,13 @@
 package com.adc.week4.data.api
 
+import com.adc.week4.data.api.response.ArticlesItem
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
-import retrofit2.http.Query
-import com.adc.week4.data.api
 
 interface NewsApi {
+    @GET("v2/top-headlines?country=id&apiKey=82f6d540990043a3a9957fcf575d19ae")
+    fun listNews(
+
+    ): Call<List<ArticlesItem>>
 
 }
